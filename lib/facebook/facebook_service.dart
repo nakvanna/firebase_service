6033a978d2c5fdb7fdaa13e24ef295fe895548e0
nakvanna <46361616+nakvanna@users.noreply.github.com>
2021-06-10 10:40:22 +0700

    Push 1.1.0 sayhello

---
+++ lib/facebook/facebook_service.dart
@@ -0,0 +1,7 @@
+part of '../firebase_service.dart';
+
+class FaceBookService {
+  sayHello() {
+    print('Hello from version 1.1.0 release');
+  }
+}
--- lib/firebase_service.dart
+++ lib/firebase_service.dart
@@ -3,5 +3,6 @@ library firebase_service;
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
+part 'facebook/facebook_service.dart';
 part 'google/google_service.dart';
 part 'phone_number_verify/phone_number_verify_service.dart';
